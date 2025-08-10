f57f22a837f45cbe1403698c7b41b52bf53742e1
Branden Leong <branden.leong.1@gmail.com>
2025-08-09 18:00:40 -0700

    Start PPO

--- games/gong_zhu/player.py
+++ games/gong_zhu/player.py
@@ -17,7 +17,6 @@ import itertools as it
 
 NUM_PLAYERS = 4
 
-
 GAME_SETTINGS = {
 	'NUM_PLAYERS': 4
 }
@@ -32,11 +31,6 @@ GAME_STATE_MAP = {
 	'LEADERBOARD':	7
 }
 
-console_listeners = []
-
-def broadcast_message_to_console_listeners(message: str):
-	for ws in console_listeners:
-		asyncio.create_task(ws.send(message))
 
 def updateEnvironmentSettings(msg: dict) -> None:
 	GAME_SETTINGS['NUM_CARDS'] =	52 * msg['data']['gameData']['numDecks']
@@ -57,27 +51,72 @@ def card_str_to_idx(card: str) -> int:
 	suit: int = suit_map(card[-1])
 	return rank + (suit * 13)
 
+class ConsoleListeners:
+	def __init__(self) -> None:
+		self.listeners = []
+		self.idx_to_name = {}
+		self.name_to_idx = {}
+
+	def add_ws(self, ws: websockets.asyncio.client.ClientConnection) -> None:
+		self.listeners.append(ws)
+
+	def remove_ws(self, ws: websockets.asyncio.client.ClientConnection) -> None:
+		self.listeners.remove(ws)
+
+	def broadcast_message(self, message: str) -> None:
+		for ws in self.listeners:
+			asyncio.create_task(ws.send(message))
+
+	def create_console(self, ws_idx: int, ws_name: str) -> None:
+		if ws_idx in self.idx_to_name.keys():
+			raise ValueError(f'ws_idx [{ws_idx}] already exists')
+		if ws_name in self.name_to_idx.keys():
+			raise ValueError(f'ws_name [{ws_name}] already exists')
+		self.idx_to_name[ws_idx] = ws_name
+		self.name_to_idx[ws_name] = ws_idx
+		self.broadcast_message(json.dumps({
+			'tag': 'createConsole',
+			'data': {'id': ws_name}
+		}))
+
 class MultiAgentEnv:
 	def __init__(self) -> None:
 		self.num_agents: int =														GAME_SETTINGS['NUM_PLAYERS']
 		self.ws_list: list[websockets.asyncio.client.ClientConnection | None] =		[None] * self.num_agents
+
 		self._latest_observation: list[dict] =										[{}] * self.num_agents
-		self._latest_state: list[dict | None] =										[None] * self.num_agents
+		self._latest_state: list[np.ndarray | None] =								[None] * self.num_agents
+		self._latest_action: list[np.ndarray | None] =								[None] * self.num_agents
 		self._latest_reward: list[float] =											[0.0] * self.num_agents
 		self._latest_done: list[bool] =												[False] * self.num_agents
 
 		self._play_history: list[np.ndarray[np.int8] | None] =						[None] * self.num_agents
 		self._leader_history: list[np.ndarray[np.int8] | None] =					[None] * self.num_agents
 
+		self._batch_ts: list[int] =													[-1] * self.num_agents
+		self._batch_states: list[np.ndarray | None] =								[None] * self.num_agents	# (B, dim(obs))
+		self._batch_actions: list[np.ndarray | None] =								[None] * self.num_agents	# (B, dim(act))
+		self._batch_log_probs: list[np.ndarray | None] =							[None] * self.num_agents	# (B,)
+		self._batch_rewards: list[np.ndarray | None] =								[None] * self.num_agents	# (E, t_per_E)
+		self._batch_reward_to_gos: list[np.ndarray | None] =						[None] * self.num_agents	# (t_per_B,)
+		self._batch_lens: list[np.ndarray | None] =									[None] * self.num_agents	# (E,)
+
+		self._episode_ts: list[int] =												[-1] * self.num_agents
+		self._episode_rewards: list[np.ndarray | None] =							[None] * self.num_agents	# (t_per_E,)
+
+		self._init_hyperparameters()
+
+	def _init_hyperparameters(self) -> None:
+		self.timesteps_per_batch: int =				4800
+		self.max_timesteps_per_episode: int =		1600
+
 	async def connect(self, url: str) -> None:
+		global console_listeners
 		async def connect_ws(ws_idx):
 			async with connect(url) as ws:
 				self.ws_list[ws_idx] = ws
 
-				broadcast_message_to_console_listeners(json.dumps({
-					'tag': 'createConsole',
-					'data': {'id': f'agent_{ws_idx}'}
-				}))
+				console_listeners.create_console(ws_idx, f'agent_{ws_idx}')
 
 				await ws.send(json.dumps({'tag': 'requestSessionID'}))
 				await self._listen(ws_idx)
@@ -86,15 +125,16 @@ class MultiAgentEnv:
 		await asyncio.gather(*tasks)
 
 	async def _listen(self, ws_idx: int) -> None:
+		global console_listeners
 		ws = self.ws_list[ws_idx]
 
 		async for message in ws:
 			msg = json.loads(message)
 			print(ws_idx, msg)
-			broadcast_message_to_console_listeners(json.dumps({
+			console_listeners.broadcast_message(json.dumps({
 				'tag': 'receiveCommand',
 				'data': {
-					'id': f'agent_{ws_idx}',
+					'id': console_listeners.idx_to_name[ws_idx],
 					'msg': [message],
 					'status': 1
 				}
@@ -121,6 +161,7 @@ class MultiAgentEnv:
 				await ws.send(json.dumps({'tag': 'joinLobby', 'data': msg['data']}))
 
 			elif msg['tag'] == 'showLobby':
+
 				if not hasattr(ws, 'connected'):
 					ws.connected = True
 
@@ -161,31 +202,78 @@ class MultiAgentEnv:
 					await ws.send(json.dumps({'tag': 'sendCommand', 'data': 'deal'}))
 
 				# print(f'===> my_idx = {msg['data']['gameData']['turnOrder'].index(ws.username)} | ws.username = {ws.username}')
-				MultiAgentEnv.encode_observation(msg['data']['gameData'], msg['data']['gameData']['turnOrder'].index(ws.username))
+				# else:
+				my_idx = GAME_SETTINGS['turn_order'].index(ws.username)
+				self.update_latest_from_gui_observation(msg['data']['gameData'], ws_idx, GAME_SETTINGS['turn_order'].index(ws.username))
+				console_listeners.broadcast_message(json.dumps({
+					'tag': 'receiveCommand',
+					'data': {
+						'id': console_listeners.idx_to_name[ws_idx],
+						'msg': [
+							f'my_idx = {GAME_SETTINGS["turn_order"].index(ws.username)}',
+							str(self._latest_observation[ws_idx]),
+							str(self._latest_state[ws_idx].shape)
+						],
+						'status': 1
+					}
+				}))
 
-	def update_state(self, idx: int, state: dict) -> None:
-		self._latest_state[idx] = state
-		# Update self._latest_reward[idx] and self._latest_done[idx]
+				if self._batch_ts[ws_idx] >= 0 and self._batch_ts[ws_idx] < self.timesteps_per_batch:
+					# TODO episode_rewards.append(latest_reward)
+					if self._episode_ts[ws_idx] >= 0 and self._episode_ts[ws_idx] < self.max_timesteps_per_episode:
+
+						self._batch_ts[ws_idx] += 1
+						self._episode_tx[ws_idx] += 1
+
+						self._batch_states[ws_idx] = np.concatenate((self._batch_states[ws_idx], self._latest_state[ws_idx][np.newaxis, ...]), axis = 0)
+						# TODO action, log_prob = get_action(self._latest_state[ws_idx])
+						# TODO batch_actions.append(action)
+						# TODO batch_log_probs.append(action)
+						# TODO act(action)
+
+					# TODO batch_lens.append(episode_t + 1)
+					# TODO batch_rewards.append(episode_rewards)
+
+				if self._latest_observation[ws_idx]['needToAct'][my_idx] == 1:
+					console_listeners.broadcast_message(json.dumps({
+						'tag': 'receiveCommand',
+						'data': {
+							'id': console_listeners.idx_to_name[ws_idx],
+							'msg': ['NEED TO ACT'],
+							'status': 0
+						}
+					}))
+					# ws.act()
 
 	async def reset(self) -> None:
-		if self._latest_state[0]['gameState'] != '':
-			await self.ws_list[0].send(json.dumps({'tag': 'sendCommand', 'data': 'exit'}))
-
-		# Sleep while any(s is not None for s in self._latest_state)
+		latest_state = self._latest_state[0]['gameState'] != ''
 
 		self._latest_observation =		[{}] * self.num_agents
-		self.latest_state =				[None] * self.agents
+		self._latest_state =			[None] * self.num_agents
+		self._latest_action =			[None] * self.num_agents
 		self._latest_reward =			[0.0] * self.num_agents
 		self._latest_done =				[False] * self.num_agents
 
-	def update_latest_from_gui_observation(self, observation: dict, idx: int) -> None:
+		if latest_state:
+			await self.ws_list[0].send(json.dumps({'tag': 'sendCommand', 'data': 'exit'}))
+		else:
+			await self.ws_list[0].send(json.dumps({'tag': 'startGame'}))
+
+		# Sleep while any(s is not None for s in self._latest_state)
+		while any(s is not None for s in self._latest_state):
+			await asyncio.sleep(0.1)
+
+	def update_latest_from_gui_observation(self, observation: dict, idx: int, turn_idx: int) -> None:
 		trick: int = round(len(observation['stacks'][0]) / len(observation['turnOrder']))
-		# encoded_observation: dict = MultiAgentEnv.encode_observation(observation, idx)
+		state: dict = MultiAgentEnv.encode_observation(observation, turn_idx)
 
 		if observation['gameState'].startswith('PLAY_'):
 			self._leader_history[idx][trick] = observation['turnFirstIdx']
 
 		self._latest_observation[idx] = observation
+		self._latest_state[idx] = state
+
+		# If latest action exists, log latest reward
 
 	def update_latest_from_console_observation(self, observation: list[str], idx: int) -> None:
 		for obs in observation:
@@ -199,13 +287,15 @@ class MultiAgentEnv:
 				player_idx: int = self._latest_observation[idx]['turnOrder'].index(username)
 				self._play_history[idx][trick][player_idx] = card_int
 
-	def get_observation(self, idx: int):		# TODO
-		# play_history
-		# leader_history
-		latest_state: dict = self._latest_state[idx]
+	async def train(self) -> None:
+		await self.reset()
+		self._batch_ts = [0] * self.num_agents
+		self._episode_ts = [0] * self.num_agents
+		self._episode_rewards = [np.empty((0,))] * self.num_agents
+		# get game state
 
 	@staticmethod
-	def encode_observation(observation: dict, my_idx: int) -> dict[str, np.ndarray]:
+	def encode_observation(observation: dict, my_idx: int) -> np.ndarray:
 
 		game_state: np.ndarray = one_hot_encode(
 			size = len(GAME_STATE_MAP),
@@ -215,7 +305,7 @@ class MultiAgentEnv:
 
 		hand: np.ndarray = one_hot_encode(
 			size = 52,
-			arr = observation['hands'][my_idx][0],
+			arr = np.array(observation['hands'][my_idx][0]),
 			dtype = np.float32
 		)
 
@@ -231,9 +321,11 @@ class MultiAgentEnv:
 
 		exposed: np.ndarray = np.ones((GAME_SETTINGS['NUM_PLAYERS'], GAME_SETTINGS['NUM_CARDS']), dtype = np.float32)
 		values: dict[int, int] = {card[0]: card[1] for card in observation['stacks'][1]}
-		for i, hand in enumerate(observation['hands']):
-			for card in hand[1]:
+		for i, _hand in enumerate(observation['hands']):
+			for card in _hand[1]:
 				exposed[i, card] = values[card]
+
+		return np.concatenate([e.flatten() for e in (game_state, hand, scores, current_trick, collected_cards, exposed)], axis = 0)
 
 		return {
 			'game_state':		game_state,
@@ -244,16 +336,42 @@ class MultiAgentEnv:
 			'exposed':			exposed
 		}
 
+	@staticmethod
+	def get_value_from_state(state: dict[str, np.ndarray], my_idx: int) -> int:
+		return state['scores'][my_idx]
+
+	@staticmethod
+	def get_reward_from_state_transition(old_state: dict[str, np.ndarray], new_state: dict[str, np.ndarray], my_idx: int) -> int:
+		return MultiAgentEnv.get_value_from_state(new_state) - MultiAgentEnv.get_value_from_state(old_state)
 
 async def console_server_handler(ws) -> None:
+	global console_listeners
+	global env
 	try:
-		console_listeners.append(ws)
+		console_listeners.add_ws(ws)
+		async for message in ws:
+			msg = json.loads(message)
+			print(msg)
+			if msg['tag'] == 'sendCommand':
+				ws_idx = console_listeners.name_to_idx[msg['id']]
+				try:
+					await env.ws_list[ws_idx].send(json.dumps(json.loads(msg['data'])))
+				except:
+					print(f'Invalid JSON string {msg["data"]}')
+			if msg['tag'] == 'reset':
+				env.reset()
+
 		await ws.wait_closed()
 	finally:
-		console_listeners.remove(ws)
+		console_listeners.remove_ws(ws)
 
 async def main() -> None:
 	url = 'ws://localhost:8080'
+
+	global console_listeners
+	global env
+
+	console_listeners = ConsoleListeners()
 
 	console_server = await serve(handler = console_server_handler, host = 'localhost', port = '8000')
 
