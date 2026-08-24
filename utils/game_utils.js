f522d34c0d4e19f59761ff78eb164be2c0f785fa
Branden Leong <branden.leong.1@gmail.com>
2026-08-23 17:48:17 -0700

    Fix Formatting

--- utils/game_utils.js
+++ utils/game_utils.js
@@ -2,31 +2,31 @@ const card2Rank = 'A 2 3 4 5 6 7 8 9 10 J Q K'.split(' ');
 const card2Suit = '♠♥♦♣'.split('');
 
 export function initDeck() {
-    return new Array(52).fill(0).map((e, i) => i);
+	return new Array(52).fill(0).map((e, i) => i);
 }
 
 export function card2Str(card) {
-    if (card == 52) return '-★';
-    if (card == 53) return '-☆';
+	if (card == 52) return '-★';
+	if (card == 53) return '-☆';
 
-    let rank = card2Rank[card % 13];
-    let suit = card2Suit[Math.floor(card / 13)];
+	let rank = card2Rank[card % 13];
+	let suit = card2Suit[Math.floor(card / 13)];
 
-    return rank + suit;
+	return rank + suit;
 }
 
 export function card2Unicode(card) {
-    if (card == 52) return String.fromCodePoint(0x1f0bf);
-    if (card == 53) return String.fromCodePoint(0x1f0df);
+	if (card == 52) return String.fromCodePoint(0x1f0bf);
+	if (card == 53) return String.fromCodePoint(0x1f0df);
 
-    const blockStart = 0x1f0a1;
+	const blockStart = 0x1f0a1;
 
-    let rank = card % 13;
-    let suit = Math.floor(card / 13);
+	let rank = card % 13;
+	let suit = Math.floor(card / 13);
 
 	if (rank >= 11) rank++;
 
-    return String.fromCodePoint(blockStart + rank + (suit * 16));
+	return String.fromCodePoint(blockStart + rank + (suit * 16));
 }
 
 export function filterByRank(card, arr) {
